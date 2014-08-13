cc3d493505e4e9d1fbc04819bae33884ae06c39e
Min-Hsien Weng <samminweng@gmail.com>
2014-08-14 05:44:24 +1200

    Iterated over a map (For_Valid_3 test case).

--- modules/wyopcl/src/wyopcl/interpreter/F/ForAllInterpreter.java
+++ modules/wyopcl/src/wyopcl/interpreter/F/ForAllInterpreter.java
@@ -4,7 +4,10 @@ import static wycc.lang.SyntaxError.internalFailure;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import wyil.lang.Code.Block;
@@ -29,6 +32,13 @@ public class ForAllInterpreter extends Interpreter {
 		return instance;
 	}
 	
+	private void gotoLoopEnd(Codes.ForAll code, StackFrame stackframe){
+		int linenumber = symboltable.get(stackframe.getBlock()).getBlockPosByLabel(code.target+"LoopEnd");
+		stackframe.setLine(linenumber);
+	}
+	
+	
+	
 	private void iterateOverListSet(Constant[] array, Codes.ForAll code, StackFrame stackframe){
 		int linenumber = stackframe.getLine();
 		Constant result = null;
@@ -50,8 +60,7 @@ public class ForAllInterpreter extends Interpreter {
 				if((index+1) >= array.length){
 					//No elements in the list.
 					stackframe.setRegister(code.indexOperand, null);
-					linenumber = symboltable.get(stackframe.getBlock()).getBlockPosByLabel(code.target+"LoopEnd");
-					stackframe.setLine(linenumber);
+					gotoLoopEnd(code, stackframe);					
 					return;
 				}else{
 					//Put the element into the register of the index operand.
@@ -63,10 +72,61 @@ public class ForAllInterpreter extends Interpreter {
 			printMessage(stackframe, code.toString(), "%"+ code.indexOperand + "("+result+")");
 			stackframe.setLine(++linenumber);
 		}else{
-			linenumber = symboltable.get(stackframe.getBlock()).getBlockPosByLabel(code.target+"LoopEnd");
-			stackframe.setLine(linenumber);
+			gotoLoopEnd(code, stackframe);			
 		}
 		
+	}
+	
+	
+	private Constant.Tuple createTuple(Entry<Constant, Constant> entry){
+		Constant.Tuple tuple = null;
+				
+		Collection<Constant> list = new ArrayList();
+		list.add(entry.getKey());
+		list.add(entry.getValue());
+		//Create a tuple
+		tuple = Constant.V_TUPLE(list);
+		
+		return tuple;
+	}
+	
+	
+	private void iterateOverMap(Constant.Map map, Codes.ForAll code, StackFrame stackframe){
+		int linenumber = stackframe.getLine();
+		Constant result = null;
+		Constant indexOperand = stackframe.getRegister(code.indexOperand);
+		
+		HashMap<Constant, Constant> values = map.values;
+		if(values.size() == 0){
+			gotoLoopEnd(code, stackframe);
+		}else{
+			Iterator<Entry<Constant, Constant>> iterator = values.entrySet().iterator();
+			if(indexOperand == null){				
+				//Create a tuple
+				result = createTuple(iterator.next());
+			}else{
+				Constant.Tuple tuple = (Constant.Tuple)indexOperand;
+				while(iterator.hasNext()){
+					//Check if the tuple matches one of the entries
+					Entry<Constant, Constant> entry = iterator.next();
+					if(entry.getKey()==tuple.values.get(0) && entry.getValue() == tuple.values.get(1)){
+						break;
+					}
+				}
+				
+				if(iterator.hasNext()){					
+					result = createTuple(iterator.next());
+				}else{
+					gotoLoopEnd(code, stackframe);
+					return;
+				}
+			}
+			
+			stackframe.setRegister(code.indexOperand, result);
+			printMessage(stackframe, code.toString(), "%"+ code.indexOperand + "("+result+")");
+			stackframe.setLine(++linenumber);
+			
+		}
 	}
 	
 
@@ -74,14 +134,13 @@ public class ForAllInterpreter extends Interpreter {
 		
 		//Get the index
 		Constant source = stackframe.getRegister(code.sourceOperand);		
-		if(source instanceof Constant.List){
-			//iterateOverList((Constant.List)source, code, stackframe);
+		if(source instanceof Constant.List){			
 			Constant.List list = (Constant.List)source;
 			Constant[] array = new Constant[list.values.size()];
 			array = list.values.toArray(array);
 			iterateOverListSet(array, code, stackframe);
-		}else if (source instanceof Constant.Map){
-			internalFailure("Not implemented!", "InterpreterForAll.java", null);
+		}else if (source instanceof Constant.Map){			
+			iterateOverMap((Constant.Map)source, code, stackframe);
 		}else if (source instanceof Constant.Set){
 			Constant.Set set = (Constant.Set)source;
 			Constant[] array = new Constant[set.values.size()];
