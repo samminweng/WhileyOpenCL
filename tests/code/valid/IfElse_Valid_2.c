6f3fce6001bfcceff365287bdf475d8c8a466fac
samminweng <samminweng@gmail.com>
2015-01-16 16:51:58 +1300

    Updated the generated C code of IfElse_Valid_2 test case.

--- tests/code/valid/IfElse_Valid_2.c
+++ tests/code/valid/IfElse_Valid_2.c
@@ -10,23 +10,20 @@ long long* clone(long long *arr, long long size){
 	}
 	return ptr;
 }
-char* toString(long long arr[], long long size, char *str){
+void append(long long* op_1, long long op_1_size, long long* op_2, long long op_2_size, long long* res){
 	long long i;
-	i=0;
-	strcpy(str, "[");
-	for(i=0;i<size;i++){
-		if(arr[i]==true){
-			strcat(str, "true");
-		}else{
-			strcat(str, "false");
-		}
-		if(i<size-1){
-			strcat(str, ", ");
-		}
+	long long res_i;
+	res_i=0;
+	for(i=0;i<op_1_size;i++){
+		res[res_i]=op_1[i];
+		res_i++;
 	}
-	strcat(str, "]");
-	return str;
+	for(i=0;i<op_2_size;i++){
+		res[res_i]=op_2[i];
+		res_i++;
+	}
 }
+
 long long f(long long _0){
 	long long _7;
 	long long _3;
@@ -71,16 +68,33 @@ int main(int argc, char** argv){
 	_3 = 1;
 	//invoke %2 = (%3) IfElse_Valid_2:f : function(int) => int
 	_2=f(_3);
+
 	//assign %1 = %2  : int
 	_1 = _2;
 	//fieldload %5 = %0 out : {[string] args,{method(any) => void print,method(any) => void println} out}
 	//fieldload %6 = %5 println : {method(any) => void print,method(any) => void println}
 	//assign %8 = %1  : int
 	_8 = _1;
+	//convert %8 = %8 any : int
 	//invoke %7 = (%8) whiley/lang/Any:toString : function(any) => string
 	sprintf(_7, "%lld", _8);
+	//convert %7 = %7 any : string
 	//indirectinvoke %6 (%7) : method(any) => void
 	printf("%s\n",_7);
 	//return
 	return -1;
+}
+char* toString(long long arr[], long long size, char *str){
+	long long i;
+	i=0;
+	strcpy(str, "[");
+	for(i=0;i<size;i++){		
+		char c[1024];
+		sprintf(c, "%d", arr[i]);
+		strcat(str, c);		if(i<size-1){
+			strcat(str, ", ");
+		}
+	}
+	strcat(str, "]");
+	return str;
 }
