83afb8169aac64db0fac38bb23e4c7dbd5b7b933
samminweng <samminweng@gmail.com>
2015-03-24 09:40:20 +1300

    Renamed the output file name.

--- tests/code/valid/benchmark.sh
+++ tests/code/valid/benchmark.sh
@@ -18,37 +18,28 @@ gcc -O0 While_Valid_1.slow.long.c -o While_Valid_1.slow.long.out
 Increments="1 5 10 50 100 200 300 400 500 1000"
 #array size starts with 1 million
 base=1000000
-for iteration in {1..3}
+for i in $Increments
 do
-	for i in $Increments
-	do
-		#Multiple the increment
-		arraysize=`expr $i \* $base`
-		echo "$arraysize"		
-		./While_Valid_1.slow.long.out $arraysize
-		#Rename the output 'result.txt'
-		cat result.txt >> result.slow.long.$arraysize.txt
-		#delete result.txt
-        rm result.txt
-	done
+	#Multiple the increment
+	arraysize=`expr $i \* $base`
+	echo "$arraysize"		
+	./While_Valid_1.slow.long.out $arraysize
+	#Rename the output 'result.txt'
+	mv result.txt result.slow.long.$arraysize.txt
 done
+
 echo "Beginning the benchmarks of slow program with long long type"
 cd ../longlong
 #compile the source C file.
 gcc -O0 While_Valid_1.slow.longlong.c -o While_Valid_1.slow.longlong.out
-for iteration in {1..3}
+for i in $Increments
 do
-        for i in $Increments
-        do
-                #Multiple the increment
-                arraysize=`expr $i \* $base`
-                echo "$arraysize"
-                ./While_Valid_1.slow.longlong.out $arraysize
-                #Append the output 'result.txt' to a text file
-                cat result.txt >> result.slow.longlong.$arraysize.txt
-                #delete result.txt
-                rm result.txt
-        done
+	#Multiple the increment
+    arraysize=`expr $i \* $base`
+    echo "$arraysize"
+    ./While_Valid_1.slow.longlong.out $arraysize
+    #Rename the output 'result.txt' 
+    mv result.txt result.slow.longlong.$arraysize.txt
 done
 #
 # Benchmark the fast program
@@ -58,36 +49,26 @@ cd ../../fast
 cd long
 #compile the C file
 gcc -O0 While_Valid_1.fast.long.c -o While_Valid_1.fast.long.out
-for iteration in {1..3}
+for i in $Increments
 do
-        for i in $Increments
-        do
-                #Multiple the increment
-                arraysize=`expr $i \* $base`
-                echo "$arraysize"
-                ./While_Valid_1.fast.long.out $arraysize
-                #Rename the output 'result.txt'
-                cat result.txt >> result.fast.long.$arraysize.txt
-                #delete result.txt
-                rm result.txt
-        done
+       #Multiple the increment
+       arraysize=`expr $i \* $base`
+       echo "$arraysize"
+       ./While_Valid_1.fast.long.out $arraysize
+       #Rename the output 'result.txt'
+       mv result.txt result.fast.long.$arraysize.txt
 done
 
 echo "Beginning the benchmarks of fast program with long long type"
 cd ../longlong
 #compile the C file
 gcc -O0 While_Valid_1.fast.longlong.c -o While_Valid_1.fast.longlong.out
-for iteration in {1..3}
+for i in $Increments
 do
-        for i in $Increments
-        do
-                #Multiple the increment
-                arraysize=`expr $i \* $base`
-                echo "$arraysize"
-                ./While_Valid_1.fast.longlong.out $arraysize
-                #Rename the output 'result.txt'
-                cat result.txt >> result.fast.longlong.$arraysize.txt
-                #delete result.txt
-                rm result.txt
-        done
+        #Multiple the increment
+        arraysize=`expr $i \* $base`
+        echo "$arraysize"
+        ./While_Valid_1.fast.longlong.out $arraysize
+        #Rename the output 'result.txt'
+        mv result.txt result.fast.longlong.$arraysize.txt
 done
