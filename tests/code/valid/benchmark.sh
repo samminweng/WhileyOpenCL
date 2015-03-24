3052acc4c81cca9964128a8fcb8b7b02a8a19c1a
samminweng <samminweng@gmail.com>
2015-03-25 10:09:24 +1300

    Added the benchmark results and output the CPU info to the result file.

--- tests/code/valid/benchmark.sh
+++ tests/code/valid/benchmark.sh
@@ -15,7 +15,7 @@ cd long
 gcc -O0 While_Valid_1.slow.long.c -o While_Valid_1.slow.long.out
 
 #Increments
-Increments="1 5 10 50 100 200 300 400 500 1000"
+Increments="1 2 3 4 5 6 7 8 9 10 11 12"
 #array size starts with 1 million
 base=1000000
 for i in $Increments
@@ -26,6 +26,8 @@ do
 	./While_Valid_1.slow.long.out $arraysize
 	#Rename the output 'result.txt'
 	mv result.txt result.slow.long.$arraysize.txt
+	#Added the CPU info
+	cat /proc/cpuinfo >> result.slow.long.$arraysize.txt
 done
 
 echo "Beginning the benchmarks of slow program with long long type"
@@ -40,6 +42,8 @@ do
     ./While_Valid_1.slow.longlong.out $arraysize
     #Rename the output 'result.txt' 
     mv result.txt result.slow.longlong.$arraysize.txt
+    #Added the CPU info
+	cat /proc/cpuinfo >> result.slow.longlong.$arraysize.txt
 done
 #
 # Benchmark the fast program
@@ -57,6 +61,8 @@ do
        ./While_Valid_1.fast.long.out $arraysize
        #Rename the output 'result.txt'
        mv result.txt result.fast.long.$arraysize.txt
+       #Added the CPU info
+	   cat /proc/cpuinfo >> result.fast.long.$arraysize.txt
 done
 
 echo "Beginning the benchmarks of fast program with long long type"
@@ -71,4 +77,6 @@ do
         ./While_Valid_1.fast.longlong.out $arraysize
         #Rename the output 'result.txt'
         mv result.txt result.fast.longlong.$arraysize.txt
+        #Added the CPU info
+	    cat /proc/cpuinfo >> result.fast.longlong.$arraysize.txt
 done
