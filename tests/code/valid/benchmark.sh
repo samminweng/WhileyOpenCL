53ea55dee16878ea051a54f2cc2986ccb14ef610
samminweng <samminweng@gmail.com>
2015-03-25 10:41:38 +1300

    Benchmarked the program with array sizes.

--- tests/code/valid/benchmark.sh
+++ tests/code/valid/benchmark.sh
@@ -15,7 +15,7 @@ cd long
 gcc -O0 While_Valid_1.slow.long.c -o While_Valid_1.slow.long.out
 
 #Increments
-Increments="1 2 3 4 5 6 7 8 9 10 11 12"
+Increments="1 5 10 20 30 40 50 60 70 80 90 100 110 120"
 #array size starts with 1 million
 base=1000000
 for i in $Increments
