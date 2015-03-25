82f029c8ebe7dcda112857e5309bb5991e7f1459
samminweng <samminweng@gmail.com>
2015-03-25 18:37:13 +1300

    Run the benchmarks with input parameters (slow/fast, long/longlong).

--- tests/code/valid/benchmark.sh
+++ tests/code/valid/benchmark.sh
@@ -3,12 +3,8 @@
 # The shell script of benchmarking the generated C code of While_Valid_1 program
 #
 run_benchmark (){
-	program=$1
-	echo $program
-	data=$2
-	echo $data
 	#Increments
-	Increments="1 5 10 20 30 40 50 60 70 80 90 100 110 120"
+	Increments="1 5 10 20 30 40 50 60 70 80 90 100 110 120 150 180 200 250 300 400 500 600 700 800 900 1000"
 	#array size starts with 1 million
 	base=1000000
 	for i in $Increments
@@ -18,18 +14,13 @@ run_benchmark (){
 		echo $arraysize		
 		./While_Valid_1.out $arraysize
 		#Rename the output 'result.txt'
-		mv result.txt result.$program.$data.$arraysize.txt
+		mv result.txt result.$1.$2.$arraysize.txt
 		#Added the CPU info
-		cat /proc/cpuinfo >> result.$program.$data.$arraysize.txt
+		cat /proc/cpuinfo >> result.$1.$2.$arraysize.txt
 	done
 }
-cd slow
 echo "Beginning the benchmarks of slow program with long type"
-#
-# Benchmark the slow C code
-#
-cd long
-run_benchmark
+cd slow/long
 #
 #compile the source C file without optimization (-O0)
 #see https://gcc.gnu.org/onlinedocs/gnat_ugn/Optimization-Levels.html#101
