98e90d9b92fa92b2cba4589270cec20fafb1b48e
samminweng <samminweng@gmail.com>
2015-04-16 10:13:39 +1200

    Added the windows script to run the benchmarks using MS Visual Studio 64bit compiler.
    As usual, the original(unoptimized) append function runs extremely
    slowly whilst the optimized one runs normally.

--- tests/code/valid/benchmark/benchmark.bat
+++ tests/code/valid/benchmark/benchmark.bat
@@ -1,28 +1,31 @@
-cd slow\longlong
-:: compiling the C program with VS x64 tool
-:cl While_Valid_1.slow.longlong.c
+@echo off
+setlocal enabledelayedexpansion
 :: test the function call
-:call :benchmark slow longlong
-REM change the directory to the benchmark directory
-cd ..\..
-cd fast\longlong
-cl While_Valid_1.fast.longlong.c
-call :benchmark fast longlong
+::call :benchmark slow longlong original
+call :benchmark fast longlong original
+::call :benchmark slow longlong optimized
+::call :benchmark fast longlong optimized
 goto :EOF
 
-
 :benchmark
-echo/%1
-echo/%2
-set arraysize=1000000
-:: Start with 10 million, finish at 120 million and increment by 10 million. 
-:loop
-While_Valid_1.%1.%2.exe %arraysize%
-::rename the output files
-ren result.txt result.%1.%2.%arraysize%.txt
-::Increase the array size
-set /a arraysize=%arraysize%*10
-if %arraysize%==10000000000 goto exit
-goto loop
-:exit
+cd %1\%2\%3
+:: compiling the C program with VS x64 tool
+cl While_Valid_1.%1.%2.%3.c
+:: Start with 10 million, 50 million, 100 million. 
+set list[0]=10000000
+set list[1]=50000000
+set list[2]=100000000
+set list[3]=120000000
+set list[4]=150000000
+set list[5]=200000000
+::read the list using a FOR /L statement
+for /l %%n in (0,1,5) do (
+	echo !list[%%n]!
+	:: run the program on the array size
+	While_Valid_1.%1.%2.%3.exe !list[%%n]!
+	::rename the output files
+	ren result.txt result.%1.%2.%3.!list[%%n]!.txt
+)
+::Change the directory to the benchmark directory
+cd ..\..\..
 goto :EOF
