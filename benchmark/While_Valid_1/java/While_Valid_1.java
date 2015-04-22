e62c9772fe4ea745e2157c528310d3659e28a45c
Min-Hsien Weng <samminweng@gmail.com>
2015-04-22 12:31:07 +1200

    Added the benchmarks of the Java program of While_Valid_1 test case.

--- benchmark/While_Valid_1/java/While_Valid_1.java
+++ benchmark/While_Valid_1/java/While_Valid_1.java
@@ -1,5 +1,9 @@
 import java.math.BigInteger;
 import java.util.Iterator;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import whiley.lang.Int;
 import wyjc.runtime.WyCollection;
 import wyjc.runtime.WyLambda;
@@ -10,7 +14,7 @@ public final class While_Valid_1
 {
   private static final BigInteger constant$0 = BigInteger.valueOf(0);
   private static final BigInteger constant$1 = BigInteger.valueOf(1);
-  private static final WyList constant$2 = tmp322_311;
+  //private static final WyList constant$2 = tmp322_311;
   
   static WyList reverse$Z9bF1D527RWz(WyList paramWyList)
   {
@@ -20,7 +24,8 @@ public final class While_Valid_1
     WyList localWyList2 = new WyList(0);
     WyList localWyList1 = localWyList2;
     WyList localWyList4;
-    for (Object localObject2 = localWyList1;; localObject2 = localWyList4)
+    Object localObject2;
+    for (localObject2 = localWyList1;; localObject2 = localWyList4)
     {
       BigInteger localBigInteger3 = constant$0;
       if (((BigInteger)localObject1).compareTo(localBigInteger3) <= 0) {
@@ -33,27 +38,31 @@ public final class While_Valid_1
       WyList localWyList3 = WyList.internal_add(new WyList(1), localBigInteger6);
       localWyList4 = WyList.append((WyList)localObject2, localWyList3);
     }
-    return localObject2;
+    return ((WyList)localObject2);
   }
   
-  static void main$2Ab737oHF4$i2$o$7C$N2Vm$7E$T0$q$7B$R2Vn$s4$a1Vw$ND$c2Vm$ch2VW$sC$R2Vt$sC$N2Vm1(WyRecord paramWyRecord)
+  //static void main$2Ab737oHF4$i2$o$7C$N2Vm$7E$T0$q$7B$R2Vn$s4$a1Vw$ND$c2Vm$ch2VW$sC$R2Vt$sC$N2Vm1(WyRecord paramWyRecord)
+  public static void main(String[] args)
   {
-    WyList localWyList2 = (WyList)WyRecord.get(paramWyRecord, "args");
-    BigInteger localBigInteger2 = WyCollection.length(localWyList2);
+    //WyList localWyList2 = (WyList)WyRecord.get(paramWyRecord, "args");
+    //BigInteger localBigInteger2 = WyCollection.length(localWyList2);
+    BigInteger localBigInteger2 = BigInteger.valueOf(args.length);
     BigInteger localBigInteger3 = constant$0;
     if (localBigInteger2.equals(localBigInteger3))
     {
-      WyRecord localWyRecord1 = (WyRecord)WyRecord.get(paramWyRecord, "out");
-      WyLambda localWyLambda1 = (WyLambda)WyRecord.get(localWyRecord1, "println");
-      WyList localWyList3 = constant$2;
-      localWyLambda1.call(new Object[] { localWyList3 });
+      //WyRecord localWyRecord1 = (WyRecord)WyRecord.get(paramWyRecord, "out");
+      //WyLambda localWyLambda1 = (WyLambda)WyRecord.get(localWyRecord1, "println");
+      System.out.println("usage: While_Valid_1 <limit>");
+      //WyList localWyList3 = constant$2;
+      //localWyLambda1.call(new Object[] { localWyList3 });
     }
     else
     {
-      WyList localWyList4 = (WyList)WyRecord.get(paramWyRecord, "args");
+      //WyList localWyList4 = (WyList)WyRecord.get(paramWyRecord, "args");
       BigInteger localBigInteger4 = constant$0;
-      WyList localWyList5 = (WyList)WyCollection.indexOf(localWyList4, localBigInteger4);
-      Object localObject4 = Int.parse$a9bF5J7J0WL1d$kS$V5$d0$Q$J5$t0kA$k5$W0VQ$R5$j$FF$B4$20FH$ZZ0kR$F6$m0FP$s5$bCWAB(localWyList5);
+      //WyList localWyList5 = (WyList)WyCollection.indexOf(localWyList4, localBigInteger4);
+      //Object localObject4 = Int.parse$a9bF5J7J0WL1d$kS$V5$d0$Q$J5$t0kA$k5$W0VQ$R5$j$FF$B4$20FH$ZZ0kR$F6$m0FP$s5$bCWAB(localWyList5);
+      Object localObject4 = new BigInteger(args[0]);
       Object localObject3 = localObject4;
       Object localObject1 = localObject3;
       if (localObject1 == null)
@@ -76,19 +85,28 @@ public final class While_Valid_1
           WyList localWyList10 = WyList.append((WyList)localObject2, localWyList9);
           localObject2 = localWyList10;
         }
-        WyList localWyList12 = reverse$Z9bF1D527RWz((WyList)localObject2);
+        WyList localWyList12 = null;
+        //Added the for-loop to measure the execution time of reverse function.
+        for(int iteration=0;iteration<10;iteration++){
+        	long startTime = System.currentTimeMillis(); 
+        	localWyList12 = reverse$Z9bF1D527RWz((WyList)localObject2);
+        	long stopTime = System.currentTimeMillis();
+            double elapsedTime = (stopTime - startTime)/ 1000.0;
+            //Write out the execution time to the text file and append the result to the existing file.
+            try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("result.txt", true)))) {
+                out.println("Array size:"+localObject1+"\tExecution time of reverse function(seconds):"+elapsedTime);
+            }catch (IOException e) {
+                //exception handling left as an exercise for the reader
+            }            
+        }
+       
         WyList localWyList11 = localWyList12;
         WyList localWyList1 = localWyList11;
-        WyRecord localWyRecord2 = (WyRecord)WyRecord.get(paramWyRecord, "out");
-        WyLambda localWyLambda2 = (WyLambda)WyRecord.get(localWyRecord2, "println");
-        localWyLambda2.call(new Object[] { localWyList1 });
+        //WyRecord localWyRecord2 = (WyRecord)WyRecord.get(paramWyRecord, "out");
+        //WyLambda localWyLambda2 = (WyLambda)WyRecord.get(localWyRecord2, "println");
+        //localWyLambda2.call(new Object[] { localWyList1 });
+        System.out.println(localWyList1);
       }
     }
   }
 }
-
-
-/* Location:           E:\GitHub\WhileyOpenCLUpgrade\benchmark\While_Valid_1\java\
- * Qualified Name:     While_Valid_1
- * JD-Core Version:    0.7.0.1
- */
--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -2,32 +2,70 @@
 #
 # The shell script of benchmarking the generated C code of While_Valid_1 program
 #
-run_benchmark (){    
-    cd $1/$2/$3
-    #
-    #compile the source C file with L2 optimization (-O2)
-    #see https://gcc.gnu.org/onlinedocs/gnat_ugn/Optimization-Levels.html#101
-    gcc -m64 -O2 -o $1.out $1.$3.c
-    #Increments
-    Increments="1 10"
-    #array size starts with 1 million
-    base=1000000
-    for i in $Increments
-    do	
-	#Multiple the increment
-	arraysize=`expr $i \* $base`
-	echo "Beginning the benchmarks of " $1 " program in " $2 " with " $3 " method on array size of " $arraysize	
-	./While_Valid_1.out $arraysize
-	#Rename the output 'result.txt'
-	mv result.txt result.$1.$2.$3.$arraysize.txt
-	#Added the CPU info
-	cat /proc/cpuinfo >> result.$1.$2.$3.$arraysize.txt
-    done
-    #Return to the working directory
-    cd ../../../
+run_benchmark_c (){    
+	cd $1/c/$2
+    	#compile the source C file with L2 optimization (-O2)
+    	#see https://gcc.gnu.org/onlinedocs/gnat_ugn/Optimization-Levels.html#101
+    	gcc -m64 -O2 -o $1.out $1.$2.c
+    	#array size
+	arraysizes="1000 10000 100000 1000000 10000000"
+	for arraysize in $arraysizes
+    	do	
+		echo "Beginning the benchmarks of " $1 " program in c with " $2 " method on array size of " $arraysize	
+		./$1.out $arraysize
+		#Rename the output 'result.txt'
+		mv result.txt result.$1.c.$2.$arraysize.txt
+		#Added the CPU info
+		cat /proc/cpuinfo >> result.$1.c.$2.$arraysize.txt
+    	done
+    	#Return to the working directory
+    	cd ../../../
 }
+#
+# The shell script of benchmarking the generated Java code of While_Valid_1 program
+#
+run_benchmark_java(){
+	# check for running under cywin
+	cygwin=false
+	case "`uname`" in
+  		CYGWIN*) cygwin=true ;;
+	esac
+	if $cygwin; then
+    		# under cygwin the classpath separator must be ";"
+    		LIBDIR=`cygpath -m "$LIBDIR"`
+    		PATHSEP=";"
+	else
+    		# under UNIX the classpath separator must be ":"
+    		PATHSEP=":"
+	fi
 
+	DIR=`dirname "$0"`/..
+	LIBDIR=$DIR/lib
+	LIBS="wyc wyil wyjc wyrl wybs wycs jasm"
+
+	. $DIR/bin/wy_common.bash
+	WHILEY_CLASSPATH=$WHILEY_CLASSPATH$PATHSEP$JAVA_HOME	
+	#echo $WHILEY_CLASSPATH
+	#change the directory to test case
+	cd $1/java
+	#compile the Java file with Javac compiler and specified class path
+	javac -classpath "$WHILEY_CLASSPATH" $1.java
+	#array size
+	arraysizes="1000 10000 100000 1000000 10000000"
+	for arraysize in $arraysizes
+    	do	
+		echo "Beginning the benchmarks of " $1 " program in Java with on array size of " $arraysize	
+		java -cp "$WHILEY_CLASSPATH" $1 $arraysize
+		#Rename the output 'result.txt'
+		mv result.txt result.$1.java.$arraysize.txt
+		#Added the CPU info
+		cat /proc/cpuinfo >> result.$1.java.$arraysize.txt
+    	done
+    	#Return to the working directory
+    	cd ../../
+}
 #
-#Benchmark the slow program
-#
-run_benchmark While_Valid_1 c slow
+#Benchmark the generated C code
+#run_benchmark_c While_Valid_1 slow
+#Benchmark the generated Java code
+run_benchmark_java While_Valid_1
