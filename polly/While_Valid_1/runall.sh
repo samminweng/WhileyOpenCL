e58f6de0df3090ebd75fa6c7149ffa1a31e031ac
sam <sam@sam-Lenovo-B590>
2015-05-26 15:43:17 +1200

    Added the fast version of While_Valid_1 C program. Also, move all the generated dot/png files to 'dot' folder.

--- polly/While_Valid_1/runall.sh
+++ polly/While_Valid_1/runall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Variable declaration
-program=While_Valid_1
+program=While_Valid_1.fast
 export PATH_TO_POLLY_LIB="/home/sam/polly/llvm_build/lib"
 alias opt="opt -load ${PATH_TO_POLLY_LIB}/LLVMPolly.so"
 
@@ -51,16 +51,30 @@ runExe(){
 		;;
 		*)
 		# Generate the assembly code using GCC
-		gcc -O2 Util.c $program.c -o $program.$optimisation.exe
+		gcc -m64 -O2 Util.c $program.c -o $program.$optimisation.exe
 		;;
 	esac
 	
     #echo "-->Time the executables"
-	start=`date +%s%N`
-	./$program.$optimisation.exe 200000000
-	end=`date +%s%N`
-	runtime=$((end-start))
-	echo "-->Program:" $1 $2 "\tExecutionTime(nanoseconds): " $runtime | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta' 
+    #Array sizes from 1 million up 100 million
+    #arraysizes="1000000 10000000 100000000"
+    arraysizes="1000000 20000000"
+    for arraysize in $arraysizes
+    	do
+    	#Repeat running the programs
+		for i in {1..10}
+		do
+			start=`date +%s%N`
+			./$program.$optimisation.exe $arraysize
+			end=`date +%s%N`
+			runtime=$((end-start))
+			echo "Iteration:"$i"\tArraysize:"$arraysize"\tExecutionTime(nanoseconds):"$runtime | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'
+			#Write the exe time to txt file
+			printf 'Arraysize:%s\tExecutionTime:%s\tnanoseconds.\n' $arraysize  $runtime >> result.$program.$optimisation.$arraysize.txt
+		done
+		#Added the CPU info
+		cat /proc/cpuinfo >> result.$program.$optimisation.$arraysize.txt
+    done
 }
 
 
@@ -77,6 +91,9 @@ opt -basicaa -polly-ast -analyze -q $program.preopt.ll
 echo "-->4. Generate SCoPs in DOT/PNG files, viewd by Graphviz"
 opt -basicaa -dot-scops -disable-output $program.preopt.ll
 for i in `ls *.dot`; do dot -Tpng $i > $i.png; done
+#Move all the *.dot and *.png to dot folder
+mkdir dot
+mv *.dot *.png dot/
 
 echo "-->5. Display polyhedral representation of SCoPs."
 opt -basicaa -polly-scops -analyze $program.preopt.ll
