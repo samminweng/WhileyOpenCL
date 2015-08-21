16247abda5166e2f0d63b7fc82cf94995892c2d4
samminweng <samminweng@gmail.com>
2015-08-21 18:24:59 +1200

    Added the function to compute in set for each block.
    The 'computeIn' takes an 'in' set and a list of byte-code, to compute
    the new in set by using the live analysis of Whiley compiler, and then
    returns the computed 'in' set as the output. 
    
    This function allows to recursively compute live variables inside an
    assertion or an invariant and overwrite 'in' set.

--- modules/wyopcl/src/wyopcl/translator/CopyEliminationAnalyzer.java
+++ modules/wyopcl/src/wyopcl/translator/CopyEliminationAnalyzer.java
@@ -10,12 +10,15 @@ import wyil.lang.Code;
 import wyil.lang.CodeBlock;
 import wyil.lang.CodeBlock.Index;
 import wyil.lang.Codes;
+import wyil.lang.Codes.Assert;
+import wyil.lang.Codes.Return;
 import wyil.lang.WyilFile;
 import wyil.lang.WyilFile.FunctionOrMethod;
 import wyil.transforms.LiveVariablesAnalysis;
 import wyil.transforms.LiveVariablesAnalysis.Env;
 import wyil.util.dfa.BackwardFlowAnalysis;
 import wyopcl.translator.bound.BasicBlock;
+import wyopcl.translator.bound.BasicBlock.BlockType;
 import wyopcl.translator.bound.CFGraph;
 
 /**
@@ -28,9 +31,8 @@ import wyopcl.translator.bound.CFGraph;
 public class CopyEliminationAnalyzer extends Analyzer {
 	private final String prefix = "%";
 	private LiveVariablesAnalysis liveAnalyzer;
-	//Store the liveness analysis for each function
-	private HashMap<FunctionOrMethod, Liveness> store;
-
+	//Store the liveness analysis for each function (Key: function name, Value:Liveness information).
+	private HashMap<String, Liveness> livenessStore;
 
 	/**
 	 * Basic Constructor
@@ -42,7 +44,7 @@ public class CopyEliminationAnalyzer extends Analyzer {
 		this.liveAnalyzer.setEnable(false);
 		this.liveAnalyzer.setNops(true);
 		//Initialize the liveness stores.
-		this.store = new HashMap<FunctionOrMethod, Liveness>();
+		this.livenessStore = new HashMap<String, Liveness>();
 	}
 
 	/**
@@ -79,131 +81,89 @@ public class CopyEliminationAnalyzer extends Analyzer {
 		return str;
 	}
 
-	/**
-	 * Apply the live variable analysis on a list of bytecode
-	 * 
-	 * @param codes
-	 */
-	/*private void applyLiveAnalysisByCode(FunctionOrMethod function) {
-		VariableDeclarations vars = function.attribute(VariableDeclarations.class);
-		String name = function.name();
-		System.out.println("###### Live analysis for " + name + " function. ######");
-		Env env = new Env();
-		List<Code> codes = function.body().bytecodes();
-		System.out.println("In" + "={" + getLiveVars(env, vars) + "}");
-		for (int iter = 0; iter < 5; iter++) {
-			System.out.println("Iteration: "+iter);
-			for (int i = codes.size() - 1; i >= 0; i--) {
-				Code code = codes.get(i);
-				// Construct an Index object.
-				CodeBlock.Index id = new CodeBlock.Index(null, i);
-				Env out = (Env) env.clone();
-				env = liveAnalyzer.propagate(id, code, env);
-				if (config.isVerbose()) {
-					System.out.println("In[" + i + "]" + "={" + getLiveVars(env, vars) + "}\n" + "L." + i + " = " + code + "\n" + "Out[" + i + "]:{"
-							+ getLiveVars(out, vars) + "}\n\n");
-				}
-				out = null;
-			}
-		}
 
-		Env out = liveAnalyzer.lastStore();
-		System.out.println("Out" + ":{" + getLiveVars(env, vars) + "}\n");
-
-	}*/
 	/**
 	 * Print out the liveness for a given function.
 	 * @param function
-	 * @param store
+	 * @param livenessStore
 	 */
 	private void printLivenss(FunctionOrMethod function){
-		VariableDeclarations vars = function.attribute(VariableDeclarations.class);
 		//Get function name
 		String name = function.name();
 		System.out.println("###### Live analysis for " + name + " function. ######");
 		//Get liveness 
-		Liveness liveness = store.get(function);
+		Liveness liveness = livenessStore.get(name);
 		//Get the list of blocks for the function.
 		List<BasicBlock> blocks = this.getBlocks(function);
 		for(BasicBlock block: blocks){
 			Env in = liveness.getInSet(block);
-			Env out = liveness.getOutSet(block);
+			Env out = liveness.getOut(block);
 			//Print out the in/out set for the block.
-			System.out.println("In" + ":{" + getLiveVars(in, function) + "}\n" + block + "\nOut" + ":{" + getLiveVars(out, function) + "}\n");
+			System.out.println("In" + ":{" + getLiveVars(in, function) + "}\n" 
+					+ block + "\nOut" + ":{" + getLiveVars(out, function) + "}\n");
 		}
 
 
 	}
 
 
-	
-	/**
-	 * Computes the live variables for return byte-code
-	 * @param code
-	 */
-	private void compute(Codes.Return code, Env in, Env out){
-		if(code.operand != Codes.NULL_REG){
-			//Check if return value is in/out set.
-			if(!in.contains(code.operand)){
-				//Add the return value to both in and out set.
-				in.add(code.operand);
-			}
-			
-			if(!out.contains(code.operand)){
-				out.add(code.operand);
-			}							
-		}
-	}
-	
-	/**
-	 * Computes live variables for invariant
-	 * @param code
-	 * @param in
-	 * @param out
-	 */
-	private void compute(Codes.Invariant code, Env in, Env out){
-		//Get the list of byte-code from invariant
-		List<Code> codes = code.bytecodes();
-		//Iterate each byte-code.
-		for(int j=codes.size()-1;j>=0;j--){
-			in = liveAnalyzer.propagate(null, codes.get(j), in);
-		}		
-	}
-	
+
 	/**
 	 * 
 	 * @param code
 	 * @param in
 	 * @param out
 	 */
-	private void compute(Codes.Invoke code, Env in, Env out){
-		//Get the callee
-	
+	private Env computeIn(Codes.Invoke code, Env in){
+		//Get the callee (called function).
+		String callee = code.name.name();
+		//Get callee's liveness 
+		Liveness liveness = livenessStore.get(callee);
+		//Check if the information has been produced.
+		if(liveness != null){
+
+		}
+		
+		return in;
+
 	}
+
 	
 	/**
-	 * Iterate each code from the block to compute the live variables.
-	 * @param codeblock the code block that contains a list of byte-code.
+	 * Compute the liveness information for a list of code.
+	 * @param block the basic block that contains a list of byte-code.
+	 * @param in the in set
+	 * @return 
 	 */
-	private void compute(CodeBlock codeBlock, Env in, Env out){
-		for (int i = codeBlock.size() - 1; i >= 0; i--) {
-			Code code = codeBlock.get(i);
-			//Compute the live variables, and store the results in in/out set.
-			//Special cases
-			if(code instanceof Codes.Return){
-				compute((Codes.Return)code, in, out);						
+	private Env computeIn(List<Code> codes, Env in){
+		// Compute the live variables, and store the results in in/out set.
+		for(int i = codes.size()-1; i>=0; i--){
+			Code code = codes.get(i);
+			if(code instanceof Codes.Assert){
+				in = computeIn(((Codes.Assert)code).bytecodes(), in);
+			}else if(code instanceof Codes.Return){
+				Return r = (Codes.Return)code;
+				if(r.operand != Codes.NULL_REG){
+					//Check if return value is in/out set.
+					if(!in.contains(r.operand)){
+						//Add the return value to both in and out set.
+						in.add(r.operand);
+					}							
+				}					
 			}else if (code instanceof Codes.Invariant){
-				compute((Codes.Invariant)code, in, out);
+				in = computeIn(((Codes.Invariant)code).bytecodes(), in);
 			}else if (code instanceof Codes.Invoke){
-				compute((Codes.Invoke)code, in, out);
-			} else {
+				in = computeIn((Codes.Invoke)code, in);
+			}else{
 				in = liveAnalyzer.propagate(null, code, in);
 			}
-		}
-		return;
-	}	
+		}		
+		return in;
+	}
+
+
 	
-	
+
 	/**
 	 * Apply live variable analysis on the function, and get in/out set of each
 	 * block.
@@ -212,10 +172,12 @@ public class CopyEliminationAnalyzer extends Analyzer {
 	 */
 	private void applyLiveAnalysisByBlock(FunctionOrMethod function) {
 		String name = function.name();
+		//Get the graph
 		CFGraph graph = this.getCFGraph(function);
 		List<BasicBlock> blocks = graph.getBlockList();
 		// Store in/out set for each block.
 		Liveness liveness = new Liveness(blocks);
+
 		int iter = 1;//Start with 1st iteration.
 		do {
 			if(config.isVerbose()){
@@ -224,29 +186,28 @@ public class CopyEliminationAnalyzer extends Analyzer {
 			}			
 			//Set the initial value of isChanged.
 			liveness.setIsChanged(false);
-			//Traverse the blocks in the reverse order.
+			//Traverse the blocks in the reverse order other than exit block
 			for (int b= blocks.size()-1;b>=0;b--) {
-				// Get the block
 				BasicBlock block = blocks.get(b);
-				// Get the child block
-				Env out = (Env)liveness.getOutSet(block).clone();
-				Env in = (Env) out.clone();
-				//Compute the live variables
-				compute(block.getCodeBlock(), in, out);				
-				// Update the in set of the block.
-				liveness.setInSet(block, in);
-				// Update the out set of the block.
-				liveness.setOutSet(block, out);				
-				if(config.isVerbose()){
-					System.out.println("In" + ":{" + getLiveVars(in, function) + "}\n"
-							+ block + "\nOut" + ":{" + getLiveVars(out, function) + "}\n");
+				if(!block.getType().equals(BlockType.EXIT)){
+					// Get in/out set of the block.
+					Env out = (Env)liveness.getOut(block).clone();
+					Env in = (Env) out.clone();
+					// Compute the store in set of the block.
+					in = computeIn(block.getCodeBlock().bytecodes(), in);					
+					// Update 'in' set of the block.
+					liveness.setInSet(block, in);	
+					if(config.isVerbose()){
+						System.out.println("In" + ":{" + getLiveVars(in, function) + "}\n"
+								+ block + "\nOut" + ":{" + getLiveVars(out, function) + "}\n");
+					}
 				}				
 			}
 			//Increment the iteration.
 			iter++;
-		}while(liveness.isChanged);
+		}while(liveness.isChanged);	
 		//Store the liveness analysis for the function. 
-		store.put(function, liveness);
+		livenessStore.put(name, liveness);
 	}
 
 	/**
@@ -260,12 +221,12 @@ public class CopyEliminationAnalyzer extends Analyzer {
 		// Apply live analysis on each function, except for main function.
 		for (FunctionOrMethod function : module.functionOrMethods()) {
 			//if(function.name().equals("main")){
-				//Print out the CFGraph
-				if(config.isVerbose()){
-					this.printCFG(function);
-				}			
-				applyLiveAnalysisByBlock(function);
-				printLivenss(function);
+			//Print out the CFGraph
+			if(config.isVerbose()){
+				this.printCFG(function);
+			}			
+			applyLiveAnalysisByBlock(function);
+			printLivenss(function);
 			//}
 		}
 
@@ -273,9 +234,9 @@ public class CopyEliminationAnalyzer extends Analyzer {
 		FunctionOrMethod main_function = module.functionOrMethod("main").get(0);
 		//Iterate each block
 		//for(BasicBlock blockthis.getBlocks(main_function)
-		
-		
-		
+
+
+
 	}
 
 	/**
@@ -287,23 +248,30 @@ public class CopyEliminationAnalyzer extends Analyzer {
 	protected class Liveness {
 		//Indicate if there is any change of in/out set.
 		private boolean isChanged;
-		private HashMap<BasicBlock, Env> inSetStore;
-		private HashMap<BasicBlock, Env> outSetStore;
+		private HashMap<BasicBlock, Env> inStore;
+		private HashMap<BasicBlock, Env> outStore;
+
+		/**
+		 * Initializes the in set for each block.
+		 * @param blocks
+		 */
+		private void initialize(List<BasicBlock> blocks){
+			//Initialize in/out set for each block.
+			for(BasicBlock block: blocks){
+				inStore.put(block, new Env());
+				outStore.put(block, new Env());
+			}
+		}
 
 		/**
 		 * Constructor with a list of blocks.
 		 * @param blocks
 		 */
 		public Liveness(List<BasicBlock> blocks) {
-			this.inSetStore = new HashMap<BasicBlock, Env>();
-			this.outSetStore = new HashMap<BasicBlock, Env>();
-			//Initialize in/out set for each block.
-			for(BasicBlock block: blocks){
-				inSetStore.put(block, new Env());
-				outSetStore.put(block, new Env());
-			}
-		}
-
+			this.inStore = new HashMap<BasicBlock, Env>();
+			this.outStore = new HashMap<BasicBlock, Env>();	
+			initialize(blocks);
+		}		
 
 		/**
 		 * Set the isChanged flag.
@@ -318,20 +286,23 @@ public class CopyEliminationAnalyzer extends Analyzer {
 		}
 
 		/**
-		 * Set 'in' set for a block.
+		 * Set 'in' set for a block and check if 'new_in' is 
+		 * different from existing 'in'. If so, then update 
+		 * 'isChanged' flag to indicate there is a change. 
 		 * 
 		 * @param block
 		 * @return
 		 */
 		protected void setInSet(BasicBlock block, Env new_in) {
 			//Check if new and existing in set are the same
-			Env in = inSetStore.get(block);
+			Env in = inStore.get(block);
 			if(!in.equals(new_in)){
 				//Use logic OR operator to combine the result of 'isChanged' flag.
-				this.isChanged |= true;
-				//Update in set
-				inSetStore.put(block, new_in);
+				this.isChanged |= true;				
 			}
+			//Update in set
+			inStore.put(block, new_in);
+
 		}
 
 		/**
@@ -340,49 +311,8 @@ public class CopyEliminationAnalyzer extends Analyzer {
 		 * @return
 		 */
 		public Env getInSet(BasicBlock block){			
-			return inSetStore.get(block);
+			return inStore.get(block);
 		}
-
-
-		/**
-		 * Set 'out' set for a block.
-		 * @param block
-		 * @param out
-		 * @return
-		 */
-		protected void setOutSet(BasicBlock block, Env new_out){
-			Env out = outSetStore.get(block);
-			if(!out.equals(new_out)){
-				this.isChanged |= true;
-				//Update the out set.
-				outSetStore.put(block, new_out);
-			}			 
-		}
-		/**
-		 * Compute the out set 
-		 * @param block
-		 * @return
-		 */
-		private void computeOutSet(BasicBlock block){
-			// Check if the block has the child blocks.
-			if (!block.isLeaf()) {
-				Env out_old = outSetStore.get(block);
-				Env out = (Env)out_old.clone();
-				// Get child nodes of the block
-				for (BasicBlock child : block.getChildNodes()) {
-					Env in = inSetStore.get(child);
-					out.addAll(in);
-				}
-
-				//Check if old and new out set are the same by using 'equal' operator.
-				if(!out.equals(out_old)){
-					this.isChanged &= true;
-					// Update the out set
-					outSetStore.put(block, out);
-				}
-			}
-		}
-
 
 
 		/**
@@ -391,9 +321,18 @@ public class CopyEliminationAnalyzer extends Analyzer {
 		 * 
 		 * @param set
 		 */
-		protected Env getOutSet(BasicBlock block) {
-			computeOutSet(block);
-			return outSetStore.get(block);
+		protected Env getOut(BasicBlock block) {
+			// Check if the block has the child blocks.
+			if (!block.isLeaf()) {
+				Env out = outStore.get(block);
+				// Get child nodes of the block
+				for (BasicBlock child : block.getChildNodes()) {
+					Env in = inStore.get(child);
+					out.addAll(in);
+				}	
+				outStore.put(block, out);
+			}
+			return outStore.get(block);
 		}
 
 	}
@@ -406,7 +345,7 @@ public class CopyEliminationAnalyzer extends Analyzer {
 	 * @author Min-Hsien Weng
 	 *
 	 */
-	protected class AliasPair{
+	/*protected class AliasPair{
 		private int left;
 		private int right;
 		private BasicBlock block;
@@ -417,7 +356,7 @@ public class CopyEliminationAnalyzer extends Analyzer {
 		}
 
 
-	}
+	}*/
 
 
 
