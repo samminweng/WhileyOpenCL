cefb1a348170f65e14786a1a36ce1c0fdd047ab0
Min-Hsien Weng <samminweng@gmail.com>
2015-09-27 15:25:24 +1300

    Added reverse test case.
    The 'reverse' function reverses array elements, and makes array copies
    twice. After the optimization, the new 'reverse' function does not use
    any array copy.

--- modules/wyopcl/test/wyopcl/testing/translator/CodeGenerationTestCase.java
+++ modules/wyopcl/test/wyopcl/testing/translator/CodeGenerationTestCase.java
@@ -84,17 +84,16 @@ public class CodeGenerationTestCase {
 		util.execCodeGeneration(codeDir, codeDir, "sort_call_by_reference", "-copy");
 	}
 	
-	
-	//@Test
-	public void test_While_Valid_1_slow() {
+	@Test
+	public void test_reverse_slow() {
 		//Generate the naive C code  
-		util.execCodeGeneration(validDir, codeDir, "While_Valid_1", "slow");
+		util.execCodeGeneration(codeDir, codeDir, "reverse");
 	}
 	
-	//@Test
-	public void test_While_Valid_1_fast() {
+	@Test
+	public void test_reverse_fast() {
 		//Generate the naive C code  
-		util.execCodeGeneration(validDir, codeDir, "While_Valid_1", "fast");
+		util.execCodeGeneration(codeDir, codeDir, "reverse", "-copy");
 	}
 	
 	//@Test
