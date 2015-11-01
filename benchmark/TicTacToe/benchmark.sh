ffc6f05c559475e11c7237c87ced7b17807500ec
sam <sam@sam-Lenovo-B590>
2015-11-01 14:44:03 +1300

    Updated benchmark results of TicTacToe test case.
    The new benchmarks set varies the number of repeats from 1 million upto
    1 billion.

--- benchmark/TicTacToe/benchmark.sh
+++ benchmark/TicTacToe/benchmark.sh
@@ -3,7 +3,7 @@
 # The shell script of benchmarking the generated Java code of Whiley program
 #
 #parameters="10 100 1000 10000 100000"
-parameters="1000"
+parameters="1000000 10000000 100000000 1000000000"
 # Large scaled parameters.
 #parameters="10 100 1000 10000 100000 1000000 10000000 100000000 200000000 300000000 400000000 500000000 600000000 700000000 800000000 900000000 1000000000"
 
@@ -79,7 +79,7 @@ run_benchmark (){
 	for parameter in $parameters
 	do
 	    #Repeat running the programs
-		for i in {1..1}
+		for i in {1..10}
 		do
 			echo "Beginning the benchmarks of $OP $SRC $CODE program method on array size =" $parameter >> $RESULT
 			start=`date +%s%N`
@@ -110,9 +110,9 @@ rm result.*.txt
 #rm -rf call_by_reference
 #
 #Benchmark the generated C code
-run_benchmark TicTacToe call_by_value CCode copy_reduced
+#run_benchmark TicTacToe call_by_value CCode copy_reduced
 run_benchmark TicTacToe call_by_value CCode copy_reduced_noleaks
 #run_benchmark TicTacToe call_by_value CCode naive
-#run_benchmark TicTacToe call_by_value CCode naive_noleaks
+run_benchmark TicTacToe call_by_value CCode naive_noleaks
 #Benchmark the generated Java code
 run_benchmark TicTacToe call_by_value JAVACode naive
