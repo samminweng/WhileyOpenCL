16aca3505bf7fec825d16b9d0f37e6f512be28de
sam <sam@sam-Lenovo-B590>
2015-11-01 16:33:26 +1300

    Added the profiling for MatrixMult test case.
    This update includes a shell script to profile optimized C program using
    Linux GCC profiling tool. Throughout profiling naive C program, we know
    the memory allocation/deallocation consumes most of execution time, and
    thus the copy reduced C code outperforms the naive one.

---
+++ benchmark/MatrixMult/profile.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+sizes="100"
+#
+# Profiling C program using Linux GCC profile tool
+# http://www.thegeekstuff.com/2012/08/gprof-tutorial/
+#
+run_gprof(){
+	NAME=$1
+	CALL=$2
+	CODE=$3
+	OP=$4
+	ProfDIR=$PWD/profile
+	mkdir -p $ProfDIR
+	DIR="$CALL/$CODE/$OP"
+	cd $DIR
+	for size in $sizes
+	do
+		# Compile c program with enabled profiling.
+		gcc -Wall -pg *.c -o "$NAME.out"
+		# Execute the file
+		"./$NAME.out" $size
+		# Run gprof tool
+		gprof -a "$NAME.out" "gmon.out" > "$ProfDIR/Analysis.$CODE.$OP.$size.txt"
+	done
+	cd ../../../
+}
+
+rm -rf $PWD/profile
+run_gprof MatrixMult call_by_value CCode copy_reduced_noleaks
+run_gprof MatrixMult call_by_value CCode naive_noleaks
