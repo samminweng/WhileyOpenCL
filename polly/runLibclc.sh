bb201e78f01e3f45a69649f4ef72befacf25e050
Min-Hsien Weng <mw169@students.waikato.ac.nz>
2016-02-25 14:13:33 +1300

    Added AMDGCN LLVM IR　code and bitcode
    Added a function to compile OpenCL kerenl code to bitcode, using Clang
    and Libclc library. But the generated bitcode has not been
    validated/tested on AMD graphic cards yet.

--- polly/runLibclc.sh
+++ polly/runLibclc.sh
@@ -39,28 +39,40 @@ compileKernelToPTX(){
 	llvm-link $LIBCLC/built_libs/nvptx64--nvidiacl.bc kernel.nvptx.ll -o kernel.nvptx.bc
 	### Compile to Ptx
 	clang -target nvptx64-nvidia-nvcl kernel.nvptx.bc -S -o kernel.nvptx.s
-	### Clean up files
-	mkdir -p "llvm"
-	mv *.ll "llvm/"
-	mkdir -p "bitcode"
-	mv *.bc "bitcode/"
 }
 
 ### Compile Kernel code to AMDGCN
 ### Reference: https://groups.google.com/forum/#!topic/llvm-dev/UH-T1CjbMEU
 compileKernelToAMDGCN(){
 	program=$1
-	read -p "Press [Enter] to compile Kernel code to AMDGCN"
-	clang -include /path/to/libclc/headers/clc.h -I /path/to/libclc/headers  -Dcl_clang_storage_class_specifiers -target amdgcn--amdhsa -mcpu=carrizo  $INPUT_FILE -o $OUTPUT_FILE
+	read -p "Press [Enter] to compile Kernel code to LLVM IR Code"
+	clang -Dcl_clang_storage_class_specifiers \
+	      -I $LIBCLC/generic/include\
+	      -include clc/clc.h\
+	      -target amdgcn -mcpu=carrizo\
+	      -x cl kernel.cl -emit-llvm -S -o kernel.amdgcn.ll
+	#clang -include /path/to/libclc/headers/clc.h -I /path/to/libclc/headers  -Dcl_clang_storage_class_specifiers -target amdgcn--amdhsa -mcpu=carrizo  $INPUT_FILE -o $OUTPUT_FILE
+	read -p "Press [Enter] to link LLVM IR Code with libclc bitcode"
+	### Link libclc bitcode and kernel code into 'kernel.linked.bc' bitcode
+	llvm-link $LIBCLC/built_libs/carrizo-amdgcn--.bc kernel.amdgcn.ll -o kernel.amdgcn.bc
+	read -p "Press [Enter] to compile to AMDGCN"
+	clang -target amdgcn -mcpu=carrizo\
+	      kernel.amdgcn.bc -S -o kernel.amdgcn.s
 }
 
 exec(){
 	program=$1 
 	cd "$program/impl/opencl"
 	#stringifyKernel $program
-	compileKernelToPTX $program
-	#compileKernelToAMDGPU $program
+	#compileKernelToPTX $program
+	compileKernelToAMDGCN $program
 	#compileOpenCLHost $program
+
+	### Clean up files
+	mkdir -p "llvm"
+	mv *.ll "llvm/"
+	mkdir -p "bitcode"
+	mv *.bc "bitcode/"
 	cd "../../.."
 }
 
