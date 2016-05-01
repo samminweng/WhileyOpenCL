3c510f98f7606b1306ab023ab252c2cb9a5368d2
Min-Hsien Weng <mw169@students.waikato.ac.nz>
2016-05-02 11:24:27 +1200

    Used Sample profiler to Profile the executables compiled by Clang
    The 'runClangSampleProfiler' function in 'runProfiler.sh' builds the C code with profiler enabled, run the executable and output sampling profiling information to 'prof' folder

--- polly/runProfiler.sh
+++ polly/runProfiler.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 ###
-## Profile the C code
+## Profile the generated C code using Gprof or Clang sample profiler
 ## 
-### ANSI escape color code used for the highlighted texts 
-GREEN="\033[1m\e[32m" # Green color
-BOLD="\033[1m" # Bold text
-REVERSE="\e[7m" #invert the foreground and background colors
-RESET="\e[0m"    # Text Reset
 ###
 TIMEOUT="1800s"
 export PATH_TO_POLLY_LIB="$HOME/polly/llvm_build/lib"
@@ -44,23 +39,75 @@ runGProf(){
 	threads=$5
 	mkdir -p "out"
 	## Compile C code with -pg option to write profile information for gprof
-	echo -e -n "${GREEN}[*]Compile $program using GCC${RESET}" && read
-	gcc -g -std=c99 -Wall -pg $program.c Util.c -o "out/$program.$compiler.enablevc.out"
+	echo -e -n "[*]Compile $program using GCC" && read
+	gcc -O3 -g -std=c99 -Wall -pg $program.c Util.c -o "out/$program.$compiler.enablevc.out"
 
 	## Execute the program
-	echo -e -n "${GREEN}[*]Run $program executable to produce profile files${RESET}" && read
+	echo -e -n "[*]Run $program executable to produce profile files" && read
 	cd out
-	"./$program.$compiler.enablevc.out" $parameter
+	time "./$program.$compiler.enablevc.out" $parameter
 
 	## The analysis file 'gmon.out' is generated
-	echo -e -n "${GREEN}[*]Run GProf${RESET}" && read
+	echo -e -n "[*]Run GProf" && read
 	mkdir -p "$PWD/../../../prof"
 	##gprof "./out/$program.$compiler.enablevc.out" gmon.out
 	analysis="$c_type.$program.$compiler.$parameter.$threads.enablevc.txt"
 	gprof "./$program.$compiler.enablevc.out" gmon.out > $analysis
+
+	echo -e -n "[*]Show Profiled Analysis of $program compiled by GCC" && read
+	cat $analysis | less
+
+	echo -e -n "[*]Complete GProf Analysis ($analysis)" && read
 	mv $analysis "$PWD/../../../prof"
-	echo -e -n "${GREEN}[*]Output GProf Analysis ($analysis) ${RESET}" && read
 	cd ..
+}
+
+
+
+###
+## Run Clang sample profiler (http://clang.llvm.org/docs/UsersManual.html#profile-guided-optimization)
+###
+runClangSampleProfiler(){
+	c_type=$1
+	program=$2
+	compiler=$3
+	parameter=$4
+	threads=$5
+	mkdir -p "out"
+	binary="$c_type.$program.$compiler.$parameter.$threads.enablevc.out"
+	sampler="$c_type.$program.$compiler.$parameter.$threads.enablevc.sampler"
+	analysis="$c_type.$program.$compiler.$parameter.$threads.enablevc.txt"
+	perfdata="$c_type.$program.$compiler.$parameter.$threads.enablevc.perf.data"
+
+	echo -e -n "[*]Build the code with source line info." && read
+	clang -O3 -g NQueens.c Util.c -o "out/$binary"
+
+	echo -e -n "[*]Run the executable with sampling profiler" && read
+	time perf record "./out/$binary" $parameter
+
+	echo -e -n "[*]Convert perf.data to LLVM's sampling profiler format using AutoFDO" && read
+	create_llvm_prof --use_lbr=false --binary="./out/$binary" --out=$sampler
+	
+	echo -e -n "[*]Show Profiled Analysis of $program compiled by Clang" && read
+	llvm-profdata show -sample $sampler -output=$analysis
+	llvm-profdata show -sample $sampler
+	
+	echo -e -n "[*]Complete Clang Sample Profiling Analysis ($analysis)" && read
+	## Clean up folder
+	mkdir -p "$PWD/../../prof"
+	mkdir -p "$PWD/../../prof/clang"
+	mkdir -p "$PWD/../../prof/clang/out"
+	mkdir -p "$PWD/../../prof/clang/sampler"
+	mkdir -p "$PWD/../../prof/clang/perfdata"
+	## Move binary file
+	mv "out/$binary" "$PWD/../../prof/clang/out"
+	## Move sample profiled data
+	mv $sampler "$PWD/../../prof/clang/sampler"
+	mv $analysis "$PWD/../../prof"
+	## Move perf.data
+	mv perf.data $perfdata
+	mv $perfdata "$PWD/../../prof/clang/perfdata"
+
 }
 
 exec(){
@@ -72,7 +119,17 @@ exec(){
 	# change folder
 	cd "$program/impl/$c_type"
 	generateCode $program $compiler
-	runGProf $c_type $program $compiler $parameter $threads
+	case "$compiler" in
+		"gcc")
+			runGProf $c_type $program $compiler $parameter $threads
+			;;
+		"clang")
+			runClangSampleProfiler $c_type $program $compiler $parameter $threads
+			;;
+		*)
+			echo -e -n "[*]Not implemented" && read
+			;;
+	esac
 
 	cd ../../..
 }
@@ -82,6 +139,9 @@ init(){
 	rm -rf "$program/prof/*.*"
 }
 
+init NQueens
+#exec autogenerate_leak NQueens gcc 13 1
+#exec autogenerate_leakfree NQueens gcc 13 1
 
-exec autogenerate_leak NQueens gcc 13 1
-exec autogenerate_leakfree NQueens gcc 13 1
+exec autogenerate_leak NQueens clang 12 1
+exec autogenerate_leakfree NQueens clang 12 1
