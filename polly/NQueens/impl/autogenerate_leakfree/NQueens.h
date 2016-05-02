de4620f6a2b2fa771864a6a0cffdb40b08da0c1f
samminweng <samminweng@gmail.com>
2016-05-02 17:11:42 +1200

    Specifies 'always_inlined' to copy_POS and free_POS function
    Althogh this change does not improve the performance, the profiled results of leak C code using Clang compiler do not explicitly call 'copy_POS' function. This is strange as C code explicitly calls the 'copy_POS' function. Perhaps the Clang inlines the copy_POS function and does not create a POS structure but updates the existing one... It would require further investigation.

--- polly/NQueens/impl/autogenerate_leakfree/NQueens.h
+++ polly/NQueens/impl/autogenerate_leakfree/NQueens.h
@@ -4,8 +4,8 @@ typedef struct{
 	long long c;
 } POS;
 void printf_POS(POS* _pos);
-POS* copy_POS(POS* _pos);
-void free_POS(POS* _pos);
-inline long long conflict(POS* p, _DECL_OWNERSHIP_PARAM(p), long long row, long long col) __attribute__((always_inline));
+inline POS* copy_POS(POS* _pos) __attribute__((always_inline));
+inline void free_POS(POS* _pos) __attribute__((always_inline));
+long long conflict(POS* p, _DECL_OWNERSHIP_PARAM(p), long long row, long long col);
 long long run(POS** queens, long long queens_size, _DECL_OWNERSHIP_PARAM(queens), long long n, long long dim);
 int main(int argc, char** args);
