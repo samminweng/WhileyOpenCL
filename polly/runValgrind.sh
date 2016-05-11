405e0ca54d9ebe6dda08ba073129494dfaf5859b
samminweng <samminweng@gmail.com>
2016-05-11 21:44:26 +1200

    Detected memory leaks on Megatron lab machine

--- polly/runValgrind.sh
+++ polly/runValgrind.sh
@@ -127,24 +127,42 @@ exec(){
 }
 ## MatrixMult test case
 init MatrixMult
-exec autogenerate_original naive MatrixMult 10
-exec autogenerate_original naive_leakfree MatrixMult 10
-exec autogenerate_original copyreduced MatrixMult 10
-exec autogenerate_original copyreduced_leakfree MatrixMult 10
+exec autogenerate_original naive MatrixMult 15
+exec autogenerate_original naive_leakfree MatrixMult 15
+exec autogenerate_original copyreduced MatrixMult 15
+exec autogenerate_original copyreduced_leakfree MatrixMult 15
+exec autogenerate_transpose naive MatrixMult 15
+exec autogenerate_transpose naive_leakfree MatrixMult 15
+exec autogenerate_transpose copyreduced MatrixMult 15
+exec autogenerate_transpose copyreduced_leakfree MatrixMult 15
 ### GCD test case
-##init GCD
-##exec autogenerate_original_leakfree GCD 1000
-##exec autogenerate_original_leakfree GCD 2000
-##exec autogenerate_cached_leakfree GCD 1000
-##exec autogenerate_cached_leakfree GCD 2000
+init GCD
+exec autogenerate_original naive GCD 100
+exec autogenerate_original naive_leakfree GCD 100
+exec autogenerate_original copyreduced GCD 100
+exec autogenerate_original copyreduced_leakfree GCD 100
+exec autogenerate_cached naive GCD 100
+exec autogenerate_cached naive_leakfree GCD 100
+exec autogenerate_cached copyreduced GCD 100
+exec autogenerate_cached copyreduced_leakfree GCD 100
+
 ### CoinGame test case
-# init CoinGame
-# exec autogenerate_leakfree CoinGame 1000
-# exec autogenerate_leakfree CoinGame 2000
-# exec autogenerate_single_leakfree CoinGame 1000
-# exec autogenerate_single_leakfree CoinGame 2000
-# exec autogenerate_array_leakfree CoinGame 1000
-# exec autogenerate_array_leakfree CoinGame 2000
-#init NQueens
-#exec autogenerate_leakfree NQueens 10
-#exec autogenerate_leakfree NQueens 12
+init CoinGame
+exec autogenerate naive CoinGame 100
+exec autogenerate naive_leakfree CoinGame 100
+exec autogenerate copyreduced CoinGame 100
+exec autogenerate copyreduced_leakfree CoinGame 100
+exec autogenerate_single naive CoinGame 100
+exec autogenerate_single naive_leakfree CoinGame 100
+exec autogenerate_single copyreduced CoinGame 100
+exec autogenerate_single copyreduced_leakfree CoinGame 100
+exec autogenerate_array naive CoinGame 100
+exec autogenerate_array naive_leakfree CoinGame 100
+exec autogenerate_array copyreduced CoinGame 100
+exec autogenerate_array copyreduced_leakfree CoinGame 100
+#### NQueen test case
+init NQueens
+exec autogenerate naive NQueens 10
+exec autogenerate naive_leakfree NQueens 10
+exec autogenerate copyreduced NQueens 10
+exec autogenerate copyreduced_leakfree NQueens 10
