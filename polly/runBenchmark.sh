b0afc53b858dfcb84e25138fcc85fd8dda182aa6
samminweng <samminweng@gmail.com>
2016-08-25 17:40:45 +1200

    Benchmarked sobel edge on small size problem

--- polly/runBenchmark.sh
+++ polly/runBenchmark.sh
@@ -129,7 +129,7 @@ exec(){
 	parameter=$3
 
 	## declare 3 kinds of code generation
-	declare -a codegens=("naive_dealloc" "copyreduced" "copyreduced_dealloc")
+	declare -a codegens=("naive" "naive_dealloc" "copyreduced" "copyreduced_dealloc")
 	## Iterate each codegen
 	for codegen in "${codegens[@]}"
 	do
@@ -197,9 +197,11 @@ init(){
 # exec MatrixMult original 12000 # Naive code runs out of memory
 
 # Sobel Edge test
-init SobelEdge
+#init SobelEdge
+exec SobelEdge original 32
+exec SobelEdge original 64
 exec SobelEdge original 128
-exec SobelEdge original 256
-exec SobelEdge original 512
-exec SobelEdge original 1024
-exec SobelEdge original 2048
+#exec SobelEdge original 256
+#exec SobelEdge original 512
+#exec SobelEdge original 1024
+#exec SobelEdge original 2048
