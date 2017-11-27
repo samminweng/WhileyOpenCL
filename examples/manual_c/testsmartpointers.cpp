3abbc163fb0d84b000b995990923f6123b770b05
samminweng <samminweng@gmail.com>
2017-11-27 15:34:58 +1300

    Updated shared pointers

--- examples/manual_c/testsmartpointers.cpp
+++ examples/manual_c/testsmartpointers.cpp
@@ -17,18 +17,25 @@ int main(int argc, char *argv[]){
   shared_ptr<Vector> p2 = p1; //Shared pointer
   
   //unique_ptr<int> p1(new int(5));
+  p2->x=5;
+  p1->x=3;
 
+  cout<<p1->x<<endl;
+  cout<<p2->x<<endl;
+
+  cout<<"----"<<endl;
   p2.reset();
   cout<<p1->x<<endl;// Still able to access the value
+  cout<<p2->x<<endl;
   p1.reset(); // Delete allocated memory
-  //cout<<p1->x<<endl;
+  cout<<p1->x<<endl;
 
-  unique_ptr<Vector> p3(new Vector);
+  /* unique_ptr<Vector> p3(new Vector);
   cout<<p3->x<<endl;
   //unique_ptr<Vector> p4 = p3; // Compiler error
   unique_ptr<Vector> p4 = move(p3);
   p3.reset();
-  cout<<p4->x<<endl;
+  cout<<p4->x<<endl;*/
   
   return 0;
 }
