8b41d7d57ddbc7e29a3f3ac471a072d63a469008
samminweng <samminweng@gmail.com>
2017-12-21 15:01:35 +1300

    Added manual-translated Sobel Edge C code

--- benchmarks/SobelEdge/manual_code/small/SobelEdge_small.c
+++ benchmarks/SobelEdge/manual_code/small/SobelEdge_small.c
@@ -2,8 +2,66 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#define MAX_LINE_LENGTH 1024*16
 const int TH = 640000;
 typedef uint8_t BYTE;
+
+// Read an image as an array of bytes
+BYTE* readPBM(FILE *file, size_t* _size){
+	char* line = malloc(MAX_LINE_LENGTH*sizeof(char));
+	size_t length = MAX_LINE_LENGTH;
+	int width = 0;
+	int height = 0;
+	// Read 'width' and 'height' from a file
+	//while(getline(&line, &length, file) != -1){
+	while(fgets(line, length, file) != NULL){
+		// Check if the line is a comment
+		if(line[0]!='#'){
+			// Read the height and width
+			sscanf(line, "%d %d\n", &width, &height);
+			break;
+		}
+	}
+	// Free 'line' as it is not used anymore
+	free(line);
+
+	size_t size = width * height;
+
+	// Allocated byte array. Note the last char (EOF)
+	BYTE* arr = (BYTE*)malloc(size*sizeof(BYTE));
+	if(arr == NULL){
+		fputs("fail to allocate the array at 'readPBM' function in Util.c\n", stderr);
+		exit(-2);
+	}
+
+	// Read a file line-by-line and pyt each byte to the array
+	size_t arr_ind = 0;
+
+	char c;
+	// Read one byte
+	while((c = getc(file)) != EOF){
+		BYTE b;
+		if(c != ' ' && c != '\n'){
+			b = (BYTE)c;
+			if(b == '1'){
+				// b is an edge, represent by black color
+				arr[arr_ind] = (BYTE)0;
+			}else if(b == '0'){
+				// b is an space, (by white color)
+				arr[arr_ind] = (BYTE)255;
+			}else{
+				arr[arr_ind] = (BYTE)b;
+			}
+			arr_ind++;
+		}
+	}
+
+	*_size = size;
+
+	return arr;
+}
+
+
 int wrap(int pos, int size){
     if(pos>=size){
         return (size -1) - (pos - size);
@@ -16,7 +74,7 @@ int wrap(int pos, int size){
     }
 }
 
-int convolution(BYTE* pixels, size_t pixels_size, int width, int height, int xCenter, int yCenter, int[] kernel){
+int convolution(BYTE* pixels, size_t pixels_size, int width, int height, int xCenter, int yCenter, int kernel[9]){
     int sum = 0;
 	int kernelSize = 3;
 	int kernelHalf = 1;
@@ -45,8 +103,8 @@ BYTE* sobelEdgeDetection(BYTE* pixels, size_t pixels_size, int width, int height
         i++;
     }
     // vertical and horizontal sobel filter (3x3 kernel)
-	int[] v_sobel = {-1,0,1,-2,0,2,-1,0,1};
-	int[] h_sobel = {1,2,1,0,0,0,-1,-2,-1};
+	int v_sobel[9]= {-1,0,1,-2,0,2,-1,0,1};
+	int h_sobel[9]= {1,2,1,0,0,0,-1,-2,-1};
 	// Perform sobel edge detection
 	int x = 0;
     while(x<width){
@@ -67,6 +125,25 @@ BYTE* sobelEdgeDetection(BYTE* pixels, size_t pixels_size, int width, int height
 }
 
 int main(int argc, char** args){
+    FILE* fp;
+    fp = fopen("../../images/input/image64x64.pbm", "r");
+    if(!fp){
+        printf("File does not exit\n");
+        exit(-2);
+    }
+    int width = 64;
+    int height = 64;
+    // Read a PBM image as a byte array
+    size_t pixels_size = 0;
+    BYTE* pixels = readPBM(fp, &pixels_size);
+    int i = 0;
+    while(i<pixels_size){
+        printf("pixels[%d]=%d",i, pixels[i]);
+        i++;
+    }
+    //size_t newPixels_size = 0;
+    //BYTE* newPixels = sobelEdgeDetection(pixels, pixels_size, width, height, &newPixels_size);
+    //printf("Blurred Image sizes: %d bytes", newPixels_size);
 
     return 0;
 }
