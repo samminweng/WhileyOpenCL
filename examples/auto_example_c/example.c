7d7c9c2cfd55b08c1de44c2c19c11184d7487013
samminweng <samminweng@gmail.com>
2018-03-25 13:18:55 +1300

    Updated the example program

--- examples/auto_example_c/example.c
+++ examples/auto_example_c/example.c
@@ -55,64 +55,71 @@ blklab1:;
 int main(int argc, char** args){
 	_DECL_1DARRAY(b);
 	_DECL_DEALLOC(b);
-	_DECL_1DARRAY(tmp);
-	_DECL_DEALLOC(tmp);
-	int64_t _3;
+	_DECL_1DARRAY(r);
+	_DECL_DEALLOC(r);
+	_DECL_1DARRAY(a);
+	_DECL_DEALLOC(a);
 	int64_t _4;
-	_DECL_1DARRAY(_5);
-	_DECL_DEALLOC(_5);
+	int64_t _5;
 	_DECL_1DARRAY(_6);
 	_DECL_DEALLOC(_6);
-	int64_t _7;
+	_DECL_1DARRAY(_7);
+	_DECL_DEALLOC(_7);
 	int64_t _8;
 	int64_t _9;
 	int64_t _10;
 	int64_t _11;
 	int64_t _12;
 	int64_t _13;
-	void* _14;
-	int64_t _16;
+	int64_t _14;
+	void* _15;
 	int64_t _17;
-	//const %3 = 2 : int
-	_3 = 2;
-	//const %4 = 3 : int
-	_4 = 3;
-	//arraygen %5 = [3; 4] : int[]
-	_DEALLOC(_5);
-	_NEW_1DARRAY_int64_t(_5, _4, _3);
-	_ADD_DEALLOC(_5);
-	//assign %1 = %5  : int[]
+	int64_t _18;
+	//const %4 = 2 : int
+	_4 = 2;
+	//const %5 = 3 : int
+	_5 = 3;
+	//arraygen %6 = [4; 5] : int[]
+	_DEALLOC(_6);
+	_NEW_1DARRAY_int64_t(_6, _5, _4);
+	_ADD_DEALLOC(_6);
+	//assign %1 = %6  : int[]
 	_DEALLOC(b);
 	// isCopyEliminated = true
-	_UPDATE_1DARRAY(b, _5);
-	_TRANSFER_DEALLOC(b, _5);
-	//const %7 = 11 : int
-	_7 = 11;
-	//invoke (%6) = (%1, %7) example:func : function(int[],int)->(int[])
+	_UPDATE_1DARRAY(b, _6);
+	_TRANSFER_DEALLOC(b, _6);
+	//const %8 = 11 : int
+	_8 = 11;
+	//invoke (%7) = (%1, %8) example:func : function(int[],int)->(int[])
 	{
 		void* tmp_b;
 		_COPY_1DARRAY_PARAM(b, tmp_b, int64_t);
-		_DEALLOC(_6);
+		_DEALLOC(_7);
 		// isCopyEliminated of '_1' = false
-		_6 = _func_(tmp_b, b_size, false, _7, _1DARRAYSIZE_PARAM_CALLBYREFERENCE(_6));
+		_7 = _func_(tmp_b, b_size, false, _8, _1DARRAYSIZE_PARAM_CALLBYREFERENCE(_7));
 		_CALLER_DEALLOC(b, "true-true-true" , "func");
-		_CALLER_DEALLOC_POST(_6, tmp_b);
+		_CALLER_DEALLOC_POST(_7, tmp_b);
 	}
-	//assign %2 = %6  : int[]
-	_DEALLOC(tmp);
+	//assign %2 = %7  : int[]
+	_DEALLOC(r);
 	// isCopyEliminated = true
-	_UPDATE_1DARRAY(tmp, _6);
-	_TRANSFER_DEALLOC(tmp, _6);
+	_UPDATE_1DARRAY(r, _7);
+	_TRANSFER_DEALLOC(r, _7);
+	//assign %3 = %2  : int[]
+	_DEALLOC(a);
+	// isCopyEliminated = true
+	_UPDATE_1DARRAY(a, r);
+	_TRANSFER_DEALLOC(a, r);
 	//assert
 	{
-		//const %8 = 0 : int
-		_8 = 0;
-		//indexof %9 = %1, %8 : int[]
-		_9=b[_8];
-		//const %10 = 2 : int
-		_10 = 2;
-		//ifeq %9, %10 goto blklab2 : int
-		if(_9==_10){goto blklab2;}
+		//const %9 = 0 : int
+		_9 = 0;
+		//indexof %10 = %1, %9 : int[]
+		_10=b[_9];
+		//const %11 = 2 : int
+		_11 = 2;
+		//ifeq %10, %11 goto blklab2 : int
+		if(_10==_11){goto blklab2;}
 		//fail
 		fprintf(stderr,"fail");
 		exit(-1);
@@ -122,14 +129,14 @@ blklab2:;
 	}
 	//assert
 	{
-		//const %11 = 0 : int
-		_11 = 0;
-		//indexof %12 = %2, %11 : int[]
-		_12=tmp[_11];
-		//const %13 = 11 : int
-		_13 = 11;
-		//ifeq %12, %13 goto blklab3 : int
-		if(_12==_13){goto blklab3;}
+		//const %12 = 0 : int
+		_12 = 0;
+		//indexof %13 = %3, %12 : int[]
+		_13=a[_12];
+		//const %14 = 11 : int
+		_14 = 11;
+		//ifeq %13, %14 goto blklab3 : int
+		if(_13==_14){goto blklab3;}
 		//fail
 		fprintf(stderr,"fail");
 		exit(-1);
@@ -137,21 +144,22 @@ blklab2:;
 blklab3:;
 	//assert
 	}
-	//fieldload %14 = %0 out : {int[][] args,{method(any)->() print,method(int[])->() print_s,method(any)->() println,method(int[])->() println_s} out}
-	//fieldload %15 = %14 println : {method(any)->() print,method(int[])->() print_s,method(any)->() println,method(int[])->() println_s}
-	//const %16 = 0 : int
-	_16 = 0;
-	//indexof %17 = %1, %16 : int[]
-	_17=b[_16];
-	//indirectinvoke () = %15 (%17) : method(any)->()
+	//fieldload %15 = %0 out : {int[][] args,{method(any)->() print,method(int[])->() print_s,method(any)->() println,method(int[])->() println_s} out}
+	//fieldload %16 = %15 println : {method(any)->() print,method(int[])->() print_s,method(any)->() println,method(int[])->() println_s}
+	//const %17 = 0 : int
+	_17 = 0;
+	//indexof %18 = %1, %17 : int[]
+	_18=b[_17];
+	//indirectinvoke () = %16 (%18) : method(any)->()
 	{
-		printf("%"PRId64"\n", _17);
+		printf("%"PRId64"\n", _18);
 	}
 	//return
 	_DEALLOC(b);
-	_DEALLOC(tmp);
-	_DEALLOC(_5);
+	_DEALLOC(r);
+	_DEALLOC(a);
 	_DEALLOC(_6);
+	_DEALLOC(_7);
 	exit(0);
 }
 
